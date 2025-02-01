79cc0b5dc7f8cf6e96ac38ab4e4f27c6b57e1ce6
needkofea <170401838+needkofea@users.noreply.github.com>
2025-02-01 19:48:47 +0800

    working on full install script

--- installer/bootstrap.sh
+++ installer/bootstrap.sh
@@ -1,3 +1,24 @@
-rust
-python3
-git
+#! /usr/bin/env bash
+
+_DIR=$( dirname -- "$0" )
+
+starter_pkgs=(
+    "python3"
+    "rust"
+    "git"
+)
+
+
+install_starter_pkgs(){
+    pacman -Sy "${starter_pkgs[@]}"
+}
+
+
+
+echo "NeedKofea bootstrap.sh"
+echo "Installing installer dependencies..."
+install_starter_pkgs
+
+
+# Run install script
+python3 $_DIR/install-all.py
