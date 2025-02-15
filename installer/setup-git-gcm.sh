133af10be33febad02b5590032c5414f961979fb
needkofea <170401838+needkofea@users.noreply.github.com.>
2025-02-15 11:06:36 +0800

    fix gcm install

--- installer/setup-git-gcm.sh
+++ installer/setup-git-gcm.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
 
-sudo pacman -Sy dotnet-runtime-8.0 --needed
+sudo pacman -Sy dotnet-sdk-8.0 --needed
 
 dotnet tool install -g git-credential-manager
+
 git-credential-manager configure
