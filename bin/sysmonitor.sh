f6312ef1b5373a3f18e65fa0b0ac1ec4c64a7bb3
needkofea <170401838+needkofea@users.noreply.github.com.>
2025-03-12 17:42:16 +0800

    fix script not working with ghostty

--- bin/sysmonitor.sh
+++ bin/sysmonitor.sh
@@ -4,4 +4,4 @@
 sysMon="btop"
 term=$(cat $HOME/.config/hypr/keybindings.conf | grep ^'$term' | cut -d '=' -f2)
 
-pkill -x "${sysMon}" || ${term} "${sysMon}" &
+pkill -x "${sysMon}" || ${term} -e "${sysMon}" &
